bc8678052423111ea15bc2923752f97126ce8371
dawndanseco <dawndanseco@gmail.com>
2024-04-29 17:01:11 +0800

    Updated, fixing the main 2

---
+++ app/src/main/java/com/example/psykitz/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.psykitz
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Set click listener for the "Continue" button
+        btContinue.setOnClickListener {
+            // Navigate to MainActivity2
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
