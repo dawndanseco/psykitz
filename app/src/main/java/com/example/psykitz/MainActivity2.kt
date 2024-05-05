319e9fd8e9d1d0b275421d12739d6c597b923418
dawndanseco <dawndanseco@gmail.com>
2024-05-05 12:56:12 +0800

    with camera

--- app/src/main/java/com/example/psykitz/MainActivity2.kt
+++ app/src/main/java/com/example/psykitz/MainActivity2.kt
@@ -1,12 +1,18 @@
 package com.example.psykitz
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
+    private var our_request_code : Int = 123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,21 @@ class MainActivity2 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun takePhoto(viewCompat: View){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(packageManager) != null){
+            startActivityForResult(intent, our_request_code)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == our_request_code && resultCode == RESULT_OK){
+            val imageView : ImageView = findViewById(R.id.ivCamera)
+            val bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(bitmap)
         }
     }
 }
